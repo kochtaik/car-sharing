ab9dd0593e20f83d3d34083cd730bcc6854779e9
kanstantsin.lazouski <kochtaik@gmail.com>
2023-07-18 08:40:45 +0200

    add '/in-use' endpoint

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,5 +1,28 @@
-function getCarsInUse(req, res) {
-    return res.json({ test: 'hello world! '})
+const Car = require('../models/Car');
+
+
+/**
+ * Obtain list of cars that are currently in use
+ * and fuel level less than 1/4 of full tank.
+ */
+async function getCarsInUse(req, res) {
+    try {
+        const car = await Car.find({
+            status: 'in-use',
+            'fuel_level.value': { $lte: 25 },
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: car,
+            nbHits: car.length,
+        });
+
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ success: false });
+    }
+    
 }
 
 function getReservedCars(req, res) {
